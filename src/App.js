8bdf78b724311f6ff22aa3d1432a307ddd8ac81f
leibGarber <steve.l.garber@gmail.com>
2020-08-10 22:08:39 +0300

    this code sucks

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          my code is out to ruin your codes day
         </a>
       </header>
     </div>
